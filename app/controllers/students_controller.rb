423f6c80dec53ee7a808b3e17a70a1d0cde53452
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-30 18:52:29 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
   def create 
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to students_path 
-    # redirect_to :action => 'new'
+    # redirect_to :action => 'new' - redirects back to new.html.erb 
   end 
 
 end
